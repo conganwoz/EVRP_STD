a702974a8e106116d0558ec8a3a771839913cf0e
AnLuubk <conganwoz@gmail.com>
2020-05-14 17:40:31 +0700

    Initial Commit

---
+++ EVRP_STD/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  EVRP_STD
+//
+//  Created by MAC on 5/14/20.
+//  Copyright © 2020 MAC. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
